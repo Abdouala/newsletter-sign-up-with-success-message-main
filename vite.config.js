8564ba8419e78e78dc4944c9c465241eabdf2371
abdouala <arrahlo@hotmail.com>
2024-02-19 14:51:32 +0000

    add modify vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
-+  base: '/vite-deploy-demo/'
++  base: '/newsletter-sign-up-with-success-message-main/'
 })
