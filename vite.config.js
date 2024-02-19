95f427fa528e6cd7d62dc30b8bd810516e96cd82
abdouala <arrahlo@hotmail.com>
2024-02-19 14:55:26 +0000

    2nd modify

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
-+  base: '/newsletter-sign-up-with-success-message-main/'
+	plugins: [react()],
+	base: '/newsletter-sign-up-with-success-message-main/'
 })
