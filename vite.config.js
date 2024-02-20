2ec46623ba510039caa8d71c9d02005138fb9893
abdouala <arrahlo@hotmail.com>
2024-02-20 09:08:59 +0000

    vite config v3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/newsletter-sign-up-with-success-message-main/'
+	base: '/newsletter-sign-up-with-success-message-main/',
+	build: {
+		rollupOptions: {
+		  input: {
+			main: resolve(__dirname, 'index.html'),
+			nested: resolve(__dirname, 'thanks.html'),
+		  },
+		},
+	},
 })
